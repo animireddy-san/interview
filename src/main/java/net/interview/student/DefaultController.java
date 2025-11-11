e0b54e8b50448a22b7264e37366776b7e3a1a91d
santosh <animireddysantosh@gmail.com>
2025-11-11 19:40:32 +0530

    main change updated

--- src/main/java/net/interview/student/DefaultController.java
+++ src/main/java/net/interview/student/DefaultController.java
@@ -8,6 +8,7 @@ public class DefaultController {
     @GetMapping("/")
     public String helloWorldEndpoint() {
         System.out.println("main change");
+        System.out.println("feate changes");
         return "Hello from Students GET / endpoint !!";
     }
 }
