cf8db26940f979d7392bcfe82fbf2571cc13d491
santosh <animireddysantosh@gmail.com>
2025-11-11 19:18:37 +0530

    first commit

--- src/main/java/net/interview/student/StudentController.java
+++ src/main/java/net/interview/student/StudentController.java
@@ -1,5 +1,12 @@
 package net.interview.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 /**
@@ -18,7 +25,18 @@ import java.util.List;
  * update or create student are via POST or PUT ? why?
  */
 
-
+@RestController
+@RequestMapping("/api/v1")
 public class StudentController {
+    @Autowired
+    private StudentRepository repository;
+
+    @GetMapping("/allstudents")
+    public ResponseEntity<List<Student>> getAllStudents()
+    {
+
+        return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+    }
+
 
 }
