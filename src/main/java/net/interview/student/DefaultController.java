0fae09de18172b28fe6abc6d0d6bd01337a7f931
santosh <animireddysantosh@gmail.com>
2025-11-11 19:22:34 +0530

    feature commit

--- src/main/java/net/interview/student/DefaultController.java
+++ src/main/java/net/interview/student/DefaultController.java
@@ -8,6 +8,7 @@ public class DefaultController {
     @GetMapping("/")
     public String helloWorldEndpoint() {
         System.out.println("enter helloWorldEndpoint");
+        System.out.println("added feature123");
         return "Hello from Students GET / endpoint !!";
     }
 }
